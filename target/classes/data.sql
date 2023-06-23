49984af9e43a1e88bc5adb5ab615d74de86bbe18
IvanM <ivan@ivan.dev>
2023-06-23 16:23:54 +0200

    Add Dockerfile

---
+++ target/classes/data.sql
@@ -0,0 +1,39 @@
+INSERT INTO PUBLISHERS (creation_timestamp, update_timestamp, name)
+VALUES(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Infinity Publications'),
+(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Live and let live Publications'),
+(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'I want to believe Publications');
+
+INSERT INTO AUTHORS (name, surname, creation_timestamp, update_timestamp)
+VALUES('Isaac', 'Newton', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Koyoharu ', 'Gotouge', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Richard', 'Helm', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Erich', 'Gamma', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Raplph', 'Johnson', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO GENRES (name, description, creation_timestamp, update_timestamp)
+VALUES('Drama','Sucesos desafortunados en la vida',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Ciencia Ficción','Literatura fantástica y narrativa de terror',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Manga','Historietas de origen japonés',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Autoayuda','Desarrollo personal y aceptación emocional',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO BOOKSHOPS (name, creation_timestamp, update_timestamp)
+VALUES('The Cornershop', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Libros, libros, libros', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Fly you fools Books', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO BOOKS (isbn, title, genre_id, publisher_id, creation_timestamp, update_timestamp)
+VALUES(1231231231233, 'Principia Mathematica', 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1231231231232, 'Kimetsu No Yaiba Vol I', 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1231231231231, 'Design patterns', 2, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO BOOKS_AUTHORS (book_id, author_id)
+VALUES(1,1),
+(2,2),
+(3,3),
+(3,4),
+(3,5);
+
+INSERT INTO BOOKS_BOOKSHOPS (book_book_id, bookshop_bookshop_id, price, units)
+VALUES(1,3,13,25),
+(2,2,9.99,10),
+(3,1,17.5,5);
