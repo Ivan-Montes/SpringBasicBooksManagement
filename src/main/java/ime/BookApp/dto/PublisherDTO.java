5162e5b08ae7d1c21fa8847b30a0897ce73dd328
IvanM <ivan.montes@outlook.com>
2023-07-22 18:05:06 +0200

    Added validations tags and message or warning

--- src/main/java/ime/BookApp/dto/PublisherDTO.java
+++ src/main/java/ime/BookApp/dto/PublisherDTO.java
@@ -2,6 +2,8 @@ package ime.BookApp.dto;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,10 @@ public class PublisherDTO implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 6923636877841433867L;
-	
+
+	@NotNull(message="{NotNull.PublisherDTO.genreId}")
 	private Long publisherId;
+
+	@Size( min = 1, max = 50, message="{Size.PublisherDTO.name")
 	private String name;	
 }
