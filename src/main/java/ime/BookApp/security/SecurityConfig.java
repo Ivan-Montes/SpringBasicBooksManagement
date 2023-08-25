6da6d492a2f3e98e3c117b63b3749cdce7b11850
IvanM <ivan.montes@outlook.com>
2023-08-25 18:46:48 +0200

    Add users in memory

--- src/main/java/ime/BookApp/security/SecurityConfig.java
+++ src/main/java/ime/BookApp/security/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -29,4 +35,27 @@ public class SecurityConfig {
 	}
 	
 
+	@Bean
+	UserDetailsService users() {
+		
+		UserDetails user = User.builder()
+			.username("user")
+			.password(passwordEncoder().encode("password"))
+			.roles("USER")
+			.build();
+		
+		UserDetails admin = User.builder()
+			.username("admin")
+			.password(passwordEncoder().encode("password"))
+			.roles("USER", "ADMIN")
+			.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+		
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
 }
