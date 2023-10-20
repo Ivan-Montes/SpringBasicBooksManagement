f76db5c4debef44baf6f920ebee86b517b8d1c23
IvanM <ivan.montes@outlook.com>
2023-10-20 16:30:09 +0200

    Rename class

---
+++ src/main/java/ime/book_app/exception/BasicInformation.java
@@ -0,0 +1,15 @@
+package ime.book_app.exception;
+
+import java.io.Serializable;
+
+import lombok.Value;
+
+@Value
+public class BasicInformation implements Serializable{
+	
+	private static final long serialVersionUID = 8509786998435141897L;
+	private String name;
+	private String message;
+	private String description;
+
+}
