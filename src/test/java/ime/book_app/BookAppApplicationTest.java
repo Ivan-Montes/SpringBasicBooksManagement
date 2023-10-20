5a6aa42f3e3a90601f33208554a70fc1e4b56cf0
IvanM <ivan.montes@outlook.com>
2023-10-20 16:30:50 +0200

    Add test main class

---
+++ src/test/java/ime/book_app/BookAppApplicationTest.java
@@ -0,0 +1,15 @@
+package ime.book_app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppApplicationTest {
+
+	@Test
+	void BookAppApplication_main_ReturnVoid() {
+		Assertions.assertTrue(true);
+	}
+
+}
