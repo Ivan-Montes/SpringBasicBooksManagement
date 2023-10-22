feacc6809fb77ad9f4be9f83368b0eeaee19ec2e
IvanM <ivan.montes@outlook.com>
2023-10-22 11:26:24 +0200

    Delete @Builder unused tag

--- src/main/java/ime/book_app/dto/AuthorCreationDTO.java
+++ src/main/java/ime/book_app/dto/AuthorCreationDTO.java
@@ -5,21 +5,22 @@ import java.io.Serializable;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Builder
+@Generated
 public class AuthorCreationDTO implements Serializable{
 
+		
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-		
+	private static final long serialVersionUID = 727075358399459951L;
+
 	@Size(min = 1, max = 50, message="{Size.AuthorCreationDTO.name}")
 	@Pattern( regexp = "[a-zA-Z\\s\\-&]+", message="{Pattern.AuthorCreationDTO.name}")
 	private String name;
