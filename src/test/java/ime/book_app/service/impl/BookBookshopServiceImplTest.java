0cf7e0261401954f611d4d1c636ca7e371445438
IvanM <ivan.montes@outlook.com>
2023-10-24 16:24:46 +0200

    Add new tests

--- src/test/java/ime/book_app/service/impl/BookBookshopServiceImplTest.java
+++ src/test/java/ime/book_app/service/impl/BookBookshopServiceImplTest.java
@@ -1,8 +1,14 @@
 package ime.book_app.service.impl;
 
 import org.assertj.core.api.Assertions;
-import static org.mockito.Mockito.doReturn;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,10 +18,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import ime.book_app.dto.BookBookshopDTO;
 import ime.book_app.entity.BookBookshop;
+import ime.book_app.entity.BookBookshopId;
 import ime.book_app.repository.BookBookshopRepository;
-import ime.book_app.service.impl.BookBookshopServiceImpl;
-
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class BookBookshopServiceImplTest {
@@ -26,6 +32,15 @@ class BookBookshopServiceImplTest {
 	@InjectMocks
 	private BookBookshopServiceImpl bookService;
 	
+	private BookBookshopId bookBookshopIdTest;
+	
+	@BeforeEach
+	private void createObjects() {
+	
+		bookBookshopIdTest = new BookBookshopId(1L,1L);
+		
+	}
+	
 	@Test
 	void BookBookshopServiceImpl_getAllBookBookshopDTO_ReturnZeroOrMoreDTO() {
 		List<BookBookshopDTO>list = List.of(Mockito.mock(BookBookshopDTO.class));
@@ -33,8 +48,10 @@ class BookBookshopServiceImplTest {
 		
 		List<BookBookshopDTO>listFound = bookService.getAllBookBookshopDTO();
 		
-		Assertions.assertThat(listFound).isNotNull();
-		Assertions.assertThat(listFound.size()).isEqualTo(1);
+		assertAll(
+				()->Assertions.assertThat(listFound).isNotNull(),
+				()->Assertions.assertThat(listFound).hasSize(1)
+				);
 	}
 	
 	@Test
@@ -44,8 +61,57 @@ class BookBookshopServiceImplTest {
 		
 		BookBookshop bbsResult = bookService.saveBookBookshop(bbs);
 		
-		Assertions.assertThat(bbsResult).isNotNull();
-		Assertions.assertThat(bbsResult).isEqualTo(bbs);
+		assertAll(
+				()->Assertions.assertThat(bbsResult).isNotNull(),
+				()->Assertions.assertThat(bbsResult).isEqualTo(bbs)
+				);
 	}
 
+	@Test
+	void BookBookshopServiceImpl_findBookBookshopById_ReturnBookBookshop() {
+		BookBookshop bbs = Mockito.mock(BookBookshop.class);
+		doReturn(Optional.of(bbs)).when(repository).findById(Mockito.any(BookBookshopId.class));
+		
+		BookBookshop bbsResult = bookService.findBookBookshopById(bookBookshopIdTest);
+		
+		assertAll(
+				()->Assertions.assertThat(bbsResult).isNotNull(),
+				()->Assertions.assertThat(bbsResult).isEqualTo(bbs)
+				);		
+	}
+	
+	@Test
+	void BookBookshopServiceImpl_findBookBookshopById_ReturnBookBookshopVoid() {
+		
+		doReturn(Optional.empty()).when(repository).findById(Mockito.any(BookBookshopId.class));
+		
+		BookBookshop bbsResult = bookService.findBookBookshopById(bookBookshopIdTest);
+		
+		assertAll(
+				()->Assertions.assertThat(bbsResult).isNotNull()
+				);		
+	}
+	
+	@Test
+	void BookBookshopServiceImpl_deleteBookBookshop_ReturnNothing() {
+		
+		doNothing().when(repository).deleteById(Mockito.any(BookBookshopId.class));
+		
+		bookService.deleteBookBookshop(bookBookshopIdTest);
+		
+		verify(repository, times(1)).deleteById(bookBookshopIdTest);
+	}
+	
+	@Test
+	void BookBookshopServiceImpl_getBookBookshopDTOById_ReturnBookBookshopDTO() {
+		BookBookshopDTO bookBookshopDTO = Mockito.mock(BookBookshopDTO.class);
+		doReturn(bookBookshopDTO).when(repository).getBookBookshopDTOById(Mockito.anyLong(), Mockito.anyLong());
+		
+		BookBookshopDTO bookBookshopDTOfound = bookService.getBookBookshopDTOById(Mockito.anyLong(), Mockito.anyLong());
+		
+		assertAll(
+				()->Assertions.assertThat(bookBookshopDTOfound).isNotNull(),
+				()->Assertions.assertThat(bookBookshopDTOfound).isEqualTo(bookBookshopDTO)
+				);
+	}
 }
