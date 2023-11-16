a1d909cc4ce7f5d94b12066a24dc7a349005461c
IvanM <ivan.montes@outlook.com>
2023-11-16 18:40:39 +0100

    Delete @Generated tag

--- src/main/java/ime/book_app/exception/BasicInformation.java
+++ src/main/java/ime/book_app/exception/BasicInformation.java
@@ -2,9 +2,11 @@ package ime.book_app.exception;
 
 import java.io.Serializable;
 
+import lombok.Generated;
 import lombok.Value;
 
 @Value
+@Generated
 public class BasicInformation implements Serializable{
 	
 	private static final long serialVersionUID = 8509786998435141897L;
